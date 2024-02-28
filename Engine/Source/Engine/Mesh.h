7ff21208174186c79a74e5593aa121f0873882f5
UVictoras <vmartinan@gamingcampus.local>
2024-02-28 10:02:00 +0100

    fixed the error

--- Engine/Source/Engine/Mesh.h
+++ Engine/Source/Engine/Mesh.h
@@ -1,27 +1,37 @@
 #pragma once
-#include "DirectX12Utils.h"
+#include "Engine.h"
+#include "Platform/Win32/d3dx12.h"
+#include "DirectX12/MathHelper.h"
 
 using namespace std;
+
+#ifndef Vertex
+
+struct Vertex {
+    DirectX::XMFLOAT3 position;
+};
+
+#endif
 
 class ENGINE_API Mesh
 {
 private:
 
-	string m_Name;
+    string m_Name;
 
-	vector<Vertex> m_VertexBuffer;
+    vector<Vertex> m_VertexBuffer;
 
-	UINT m_VertexBufferSize;
+    UINT m_VertexBufferSize;
 
 public:
 
-	Mesh(string name);
+    Mesh(string name);
 
-	~Mesh();
+    ~Mesh();
 
-	void InitializeMesh(DirectX::XMFLOAT3 vertex1, DirectX::XMFLOAT3 vertex2, DirectX::XMFLOAT3 vertex3);
+    void InitializeMesh(DirectX::XMFLOAT3 vertex1, DirectX::XMFLOAT3 vertex2, DirectX::XMFLOAT3 vertex3);
 
-	vector<Vertex> GetBuffer();
+    vector<Vertex> GetBuffer();
 
-	UINT GetBufferSize();
+    UINT GetBufferSize();
 };
