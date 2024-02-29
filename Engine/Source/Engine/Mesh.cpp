6737f76cb68cc8f883c243050c6220070d3a1b28
barhancet <barhancet@GCFRLY247>
2024-02-29 15:59:49 +0100

    LA mesh avance

--- Engine/Source/Engine/Mesh.cpp
+++ Engine/Source/Engine/Mesh.cpp
@@ -17,15 +17,80 @@ void Mesh::InitializeMesh(DirectX::XMFLOAT3 vertex1, DirectX::XMFLOAT3 vertex2, 
     m_Vertices.push_back({ vertex2 });
     m_Vertices.push_back({ vertex3 });
 
-	m_VerticesSize = sizeof(m_Vertices);
+    m_VertexSize = sizeof(vertex1);
+	m_VerticesCount = sizeof(m_Vertices);
 }
 
-UINT* Mesh::GetVerticesSize()
+void Mesh::CreateVertexBuffer(ID3D12Device* device) {
+
+    // Créer un tampon de ressources (vertex buffer)
+    CD3DX12_HEAP_PROPERTIES heapProperties(D3D12_HEAP_TYPE_DEFAULT);
+    CD3DX12_RESOURCE_DESC resourceDesc = CD3DX12_RESOURCE_DESC::Buffer(m_VertexSize * m_VerticesCount);
+    device->CreateCommittedResource(
+        &heapProperties,
+        D3D12_HEAP_FLAG_NONE,
+        &resourceDesc,
+        D3D12_RESOURCE_STATE_COPY_DEST,
+        nullptr,
+        IID_PPV_ARGS(&m_VertexBuffer)
+    );
+
+    // Remplir le tampon de ressources avec les données des vertices
+    UINT8* pVertexDataBegin;
+    CD3DX12_RANGE readRange(0, 0); // On ne lit pas les données actuellement, donc la plage est vide
+    m_VertexBuffer->Map(0, &readRange, reinterpret_cast<void**>(&pVertexDataBegin));
+    memcpy(pVertexDataBegin, ConvertVectorConst(m_Vertices), m_VertexSize * m_VerticesCount);
+    m_VertexBuffer->Unmap(0, nullptr);
+
+    return;
+
+};
+
+void Mesh::CreateIndexBuffer(ID3D12Device* device) {
+
+    // Créer un tampon de ressources (index buffer)
+    CD3DX12_HEAP_PROPERTIES heapProperties(D3D12_HEAP_TYPE_DEFAULT);
+    CD3DX12_RESOURCE_DESC resourceDesc = CD3DX12_RESOURCE_DESC::Buffer(m_IndexCount * m_IndexSize);
+    device->CreateCommittedResource(
+        &heapProperties,
+        D3D12_HEAP_FLAG_NONE,
+        &resourceDesc,
+        D3D12_RESOURCE_STATE_COPY_DEST,
+        nullptr,
+        IID_PPV_ARGS(&m_IndicesBuffer)
+    );
+
+    // Remplir le tampon de ressources avec les données des indices
+    UINT8* pIndexDataBegin;
+    CD3DX12_RANGE readRange(0, 0); // On ne lit pas les données actuellement, donc la plage est vide
+    m_IndicesBuffer->Map(0, &readRange, reinterpret_cast<void**>(&pIndexDataBegin));
+    memcpy(pIndexDataBegin, indices, indexCount * indexSize);
+    m_IndicesBuffer->Unmap(0, nullptr);
+
+    return;
+};
+
+void Mesh::CreateVertexBufferView(ID3D12Device* device) 
 {
-	return &m_VerticesSize;
+ 
+    // Remplir la structure D3D12_VERTEX_BUFFER_VIEW
+    m_VertexBufferView.BufferLocation = m_Vertices.GetGPUVirtualAddress(); // Adresse virtuelle du vertex buffer
+    m_VertexBufferView.SizeInBytes = m_vertexBufferData.vertexCount * m_vertexBufferData.stride; // Taille totale du vertex buffer en octets
+    m_VertexBufferView.StrideInBytes = m_vertexBufferData.stride; // Taille d'un vertex en octets
+
+    return;
 }
 
-vector<Vertex>* Mesh::GetVertices()
+void Mesh::CreateIndexBufferView(ID3D12Device* device)
 {
-    return &m_Vertices;
-}
+    // Remplir la structure D3D12_INDEX_BUFFER_VIEW
+    m_IndexBufferView.BufferLocation = m_indexBuffer->GetGPUVirtualAddress(); // Adresse virtuelle de l'index buffer
+    m_IndexBufferView.Format = DXGI_FORMAT_R32_UINT; // Format des indices (par exemple, 32 bits non signés)
+    m_IndexBufferView.SizeInBytes = sizeof(UINT) * m_indexBufferData.indexCount; // Taille totale de l'index buffer en octets
+
+    return;
+};
+
+const Vertex* Mesh::ConvertVectorConst(vector<Vertex> vertices) {
+    return vertices.data();
+};
--- Engine/Source/Engine/Mesh.h
+++ Engine/Source/Engine/Mesh.h
@@ -4,6 +4,7 @@
 #include "DirectX12/MathHelper.h"
 
 using namespace std;
+using Microsoft::WRL::ComPtr;
 
 #ifndef Vertex
 
@@ -19,19 +20,60 @@ private:
 
     string m_Name;
 
-    vector<Vertex> m_Vertices;
+    vector<Vertex>                  m_Vertices;
+    UINT                            m_VertexSize;//Taille d'un vertex en octet
+    UINT                            m_VerticesCount;//Nombre de vertex dans le tableau
+    ComPtr<ID3D12Resource>          m_VertexBuffer;
+    D3D12_INDEX_BUFFER_VIEW         m_VertexBufferView = {};
 
-    UINT m_VerticesSize;
+    vector<UINT>                    m_Indices;
+    UINT                            m_IndexSize;//Taille d'un vertex en octet
+    UINT                            m_IndexCount;
+    ComPtr<ID3D12Resource>          m_IndicesBuffer;
+    D3D12_INDEX_BUFFER_VIEW         m_IndexBufferView = {};
 
 public:
 
-    Mesh(string name);
+    /*
+    * |----------------------------------------------
+    * |	                    Init                     |
+    * |----------------------------------------------
+    */
 
+    Mesh(string name);
     ~Mesh();
 
     void InitializeMesh(DirectX::XMFLOAT3 vertex1, DirectX::XMFLOAT3 vertex2, DirectX::XMFLOAT3 vertex3);
+    void CreateVertexBuffer(ID3D12Device* device);
+    void CreateIndexBuffer(ID3D12Device* device);
+    void CreateVertexBufferView(ID3D12Device* device);
+    void CreateIndexBufferView(ID3D12Device* device);
 
-    UINT* GetVerticesSize();
+    const Vertex* ConvertVectorConst(vector<Vertex> vertices);
 
-    vector<Vertex>* GetVertices();
+    /*
+    * |----------------------------------------------
+    * |	               Getters/Setter                |
+    * |----------------------------------------------
+    */
+
+    UINT* GetVerticesSize() { return &m_VertexSize; };
+    vector<Vertex>* GetVertices() { return &m_Vertices; };
+    D3D12_INDEX_BUFFER_VIEW* GetVertexBufferView() { return &m_VertexBufferView; };
+
+    UINT* GetIndexCount() { return &m_IndexCount; };
+    vector<UINT>* GetIndices() { return &m_Indices; };
+    D3D12_INDEX_BUFFER_VIEW* GetIndexBufferView() { return &m_IndexBufferView; } ;
+
+    //////////////////////////////////////////////////////////////////////////////////
+
+    void AddVertices(Vertex vertices) { m_Vertices.push_back(vertices); m_VerticesCount++;};
+    void SetVertexBufferView(D3D12_INDEX_BUFFER_VIEW vertexBufferView) { m_VertexBufferView = vertexBufferView; };
+
+    void AddIndices(UINT indice) { m_Indices.push_back(indice); m_IndexCount++;};
+    void SetIndexBufferView(D3D12_INDEX_BUFFER_VIEW indexBufferView) { m_IndexBufferView = indexBufferView; };
+
+
+
+    
 };
