76bc3e02a1aae7d34c86b6c41b4097e8713a5349
UVictoras <vmartinan@gamingcampus.local>
2024-03-07 15:02:06 +0100

    no more windows float but XMFLOAT3

--- Engine/Source/Engine/Atom.h
+++ Engine/Source/Engine/Atom.h
@@ -3,16 +3,15 @@
 #include "Engine/transform.h"
 #include "Engine/CubeMesh.h"
 #include "Engine/Entity.h"
-#include <Windowsnumerics.h>
 
-using namespace Windows::Foundation::Numerics;
+using namespace DirectX;
 
 class ENGINE_API Atom : public Entity
 { //Créer un CubeMesh dans l'atom afin de pouvoir le draw ultérieurement
 public:
 	float m_LifeTime;//Life time of the atom
 	float m_Speed;//Movement speed of the atom
-	float3 m_Direction;//Vector made of float coordinates for the direction of the atom
+	XMFLOAT3 m_Direction;//Vector made of float coordinates for the direction of the atom 
 
 	Atom(ID3D12Device* device);
 	virtual ~Atom();
