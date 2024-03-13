4190464644f4555b6f00c17f3f204fbe553fc1f1
barhancet <barhancet@GCFRLY247>
2024-03-13 16:32:26 +0100

    skybox fix

--- Engine/Source/Engine/Skybox.h
+++ Engine/Source/Engine/Skybox.h
@@ -72,35 +72,35 @@ struct ENGINE_API SkyBox {
 
 	void GenerateSkybox() 
 	{
-		skybox[0] = Vertex({ XMFLOAT3(-100.0f, 100.0f, -100.0f), XMFLOAT4(Colors::White),XMFLOAT2(0,0) });//0
-		skybox[1] = Vertex({ XMFLOAT3(100.0f, 100.0f, -100.0f), XMFLOAT4(Colors::White),XMFLOAT2(0,1) });//1
-		skybox[2] = Vertex({ XMFLOAT3(100.0f, -100.0f, -100.0f), XMFLOAT4(Colors::Black),XMFLOAT2(1,1) });//2
-		skybox[3] = Vertex({ XMFLOAT3(-100.0f, -100.0f, -100.0f), XMFLOAT4(Colors::Black),XMFLOAT2(1,0) });//3
+		skybox[0] = Vertex({ XMFLOAT3(-100.0f, 100.0f, -100.0f), XMFLOAT4(Colors::White),XMFLOAT2(1,0) });//0 0 0
+		skybox[1] = Vertex({ XMFLOAT3(100.0f, 100.0f, -100.0f), XMFLOAT4(Colors::White),XMFLOAT2(0,0) });//1 0 1
+		skybox[2] = Vertex({ XMFLOAT3(100.0f, -100.0f, -100.0f), XMFLOAT4(Colors::Black),XMFLOAT2(0,1) });//2 1 1
+		skybox[3] = Vertex({ XMFLOAT3(-100.0f, -100.0f, -100.0f), XMFLOAT4(Colors::Black),XMFLOAT2(1,1) });//3 1 0
 
-		skybox[4] = Vertex({ XMFLOAT3(-100.0f, 100.0f, -100.0f), XMFLOAT4(Colors::White),XMFLOAT2(0,0) });//4
-		skybox[5] = Vertex({ XMFLOAT3(-100.0f, 100.0f, 100.0f), XMFLOAT4(Colors::White),XMFLOAT2(0,1) });//5
-		skybox[6] = Vertex({ XMFLOAT3(-100.0f, -100.0f, 100.0f), XMFLOAT4(Colors::Black) ,XMFLOAT2(1,1) });//6
-		skybox[7] = Vertex({ XMFLOAT3(-100.0f, -100.0f, -100.0f), XMFLOAT4(Colors::Black),XMFLOAT2(1,0) });//7
+		skybox[4] = Vertex({ XMFLOAT3(-100.0f, 100.0f, -100.0f), XMFLOAT4(Colors::White),XMFLOAT2(1,0) });//4
+		skybox[5] = Vertex({ XMFLOAT3(-100.0f, 100.0f, 100.0f), XMFLOAT4(Colors::White),XMFLOAT2(0,0) });//5
+		skybox[6] = Vertex({ XMFLOAT3(-100.0f, -100.0f, 100.0f), XMFLOAT4(Colors::Black) ,XMFLOAT2(0,1) });//6
+		skybox[7] = Vertex({ XMFLOAT3(-100.0f, -100.0f, -100.0f), XMFLOAT4(Colors::Black),XMFLOAT2(1,1) });//7
 
-		skybox[8] = Vertex({ XMFLOAT3(-100.0f, -100.0f, 100.0f), XMFLOAT4(Colors::White), XMFLOAT2(0,0) });//8
-		skybox[9] = Vertex({ XMFLOAT3(100.0f, -100.0f, 100.0f), XMFLOAT4(Colors::White), XMFLOAT2(0,1) });//9
-		skybox[10] = Vertex({ XMFLOAT3(100.0f, 100.0f, 100.0f), XMFLOAT4(Colors::Black), XMFLOAT2(1,1) });//10
-		skybox[11] = Vertex({ XMFLOAT3(-100.0f, 100.0f, 100.0f), XMFLOAT4(Colors::Black), XMFLOAT2(1,0) });//11
+		skybox[8] = Vertex({ XMFLOAT3(-100.0f, -100.0f, 100.0f), XMFLOAT4(Colors::White), XMFLOAT2(0,1) });//8
+		skybox[9] = Vertex({ XMFLOAT3(100.0f, -100.0f, 100.0f), XMFLOAT4(Colors::White), XMFLOAT2(1,1) });//9
+		skybox[10] = Vertex({ XMFLOAT3(100.0f, 100.0f, 100.0f), XMFLOAT4(Colors::Black), XMFLOAT2(1,0) });//10
+		skybox[11] = Vertex({ XMFLOAT3(-100.0f, 100.0f, 100.0f), XMFLOAT4(Colors::Black), XMFLOAT2(0,0) });//11
 
-		skybox[12] = Vertex({ XMFLOAT3(100.0f, 100.0f, 100.0f), XMFLOAT4(Colors::White),XMFLOAT2(0,0) });//12
-		skybox[13] = Vertex({ XMFLOAT3(100.0f, 100.0f, -100.0f), XMFLOAT4(Colors::White),XMFLOAT2(0,1) });//13
-		skybox[14] = Vertex({ XMFLOAT3(100.0f, -100.0f, -100.0f), XMFLOAT4(Colors::Black),XMFLOAT2(1,1) });//14
-		skybox[15] = Vertex({ XMFLOAT3(100.0f, -100.0f, 100.0f), XMFLOAT4(Colors::Black) ,XMFLOAT2(1,0) });//15
+		skybox[12] = Vertex({ XMFLOAT3(100.0f, 100.0f, 100.0f), XMFLOAT4(Colors::White),XMFLOAT2(1,0) });//12
+		skybox[13] = Vertex({ XMFLOAT3(100.0f, 100.0f, -100.0f), XMFLOAT4(Colors::White),XMFLOAT2(0,0) });//13
+		skybox[14] = Vertex({ XMFLOAT3(100.0f, -100.0f, -100.0f), XMFLOAT4(Colors::Black),XMFLOAT2(0,1) });//14
+		skybox[15] = Vertex({ XMFLOAT3(100.0f, -100.0f, 100.0f), XMFLOAT4(Colors::Black) ,XMFLOAT2(1,1) });//15
 
-		skybox[16] = Vertex({ XMFLOAT3(-100.0f, 100.0f, 100.0f), XMFLOAT4(Colors::White),XMFLOAT2(0,0) });//16
-		skybox[17] = Vertex({ XMFLOAT3(-100.0f, 100.0f, -100.0f), XMFLOAT4(Colors::White),XMFLOAT2(0,1) });//17
-		skybox[18] = Vertex({ XMFLOAT3(100.0f, 100.0f, -100.0f), XMFLOAT4(Colors::Black) ,XMFLOAT2(1,1) }); // 18
-		skybox[19] = Vertex({ XMFLOAT3(100.0f, 100.0f, 100.0f), XMFLOAT4(Colors::Black),XMFLOAT2(1,0) });//19
+		skybox[16] = Vertex({ XMFLOAT3(-100.0f, 100.0f, 100.0f), XMFLOAT4(Colors::White),XMFLOAT2(1,0) });//16
+		skybox[17] = Vertex({ XMFLOAT3(-100.0f, 100.0f, -100.0f), XMFLOAT4(Colors::White),XMFLOAT2(0,0) });//17
+		skybox[18] = Vertex({ XMFLOAT3(100.0f, 100.0f, -100.0f), XMFLOAT4(Colors::Black) ,XMFLOAT2(0,1) }); // 18
+		skybox[19] = Vertex({ XMFLOAT3(100.0f, 100.0f, 100.0f), XMFLOAT4(Colors::Black),XMFLOAT2(1,1) });//19
 
-		skybox[20] = Vertex({ XMFLOAT3(-100.0f, -100.0f, 100.0f), XMFLOAT4(Colors::White),XMFLOAT2(0,0) });//20
-		skybox[21] = Vertex({ XMFLOAT3(-100.0f, -100.0f, -100.0f), XMFLOAT4(Colors::White),XMFLOAT2(0,1) });//21
-		skybox[22] = Vertex({ XMFLOAT3(100.0f, -100.0f, -100.0f), XMFLOAT4(Colors::Black) ,XMFLOAT2(1,1) });//22
-		skybox[23] = Vertex({ XMFLOAT3(100.0f, -100.0f, 100.0f), XMFLOAT4(Colors::Black),XMFLOAT2(1,0) });//23
+		skybox[20] = Vertex({ XMFLOAT3(-100.0f, -100.0f, 100.0f), XMFLOAT4(Colors::White),XMFLOAT2(1,0) });//20
+		skybox[21] = Vertex({ XMFLOAT3(-100.0f, -100.0f, -100.0f), XMFLOAT4(Colors::White),XMFLOAT2(0,0) });//21
+		skybox[22] = Vertex({ XMFLOAT3(100.0f, -100.0f, -100.0f), XMFLOAT4(Colors::Black) ,XMFLOAT2(0,1) });//22
+		skybox[23] = Vertex({ XMFLOAT3(100.0f, -100.0f, 100.0f), XMFLOAT4(Colors::Black),XMFLOAT2(1,1) });//23
 	};
 
 #pragma endregion
