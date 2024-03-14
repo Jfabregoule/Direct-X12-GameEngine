04e7139028888601e2f5e04d41ceb8aaadd3f1ca
loanalix <lalix@gaming.tech>
2024-03-14 22:18:40 +0100

    laser changer + test mais je veux pas tout casser

--- Engine/Source/DirectX12/dx12Inst.cpp
+++ Engine/Source/DirectX12/dx12Inst.cpp
@@ -292,6 +292,7 @@ VOID DirectX12Instance::InitTextures() {
     m_pTextureManager->AddTexture("victor", L"Content/Images/image.dds");
     m_pTextureManager->AddTexture("sky", L"Content/Images/sky.dds");
     m_pTextureManager->AddTexture("laser", L"Content/Images/laser.dds");
+    m_pTextureManager->AddTexture("bullet", L"Content/Images/or.dds");
     
 }
 
