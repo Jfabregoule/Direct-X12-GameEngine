184de45d94a9b3b783bd5f94f07aca9d1ece5ea0
barhancet <barhancet@GCFRLY008>
2024-03-14 18:33:11 +0100

    MeshRenderer

--- Engine/Source/Engine/MeshRenderer.cpp
+++ Engine/Source/Engine/MeshRenderer.cpp
@@ -14,10 +14,11 @@
 #pragma region Constructor And Destructor
 
 MeshRenderer::MeshRenderer() {
-		
+
 };
 
 MeshRenderer::~MeshRenderer() {
+	//m_pConstantBufferGPU->Unmap(0, nullptr);
 	m_pMesh = nullptr;
 	m_pShader = nullptr;
 	delete m_pConstantBufferData;
@@ -42,7 +43,8 @@ void MeshRenderer::InitMeshRenderer(DirectX12Instance* inst, std::string type, s
 	m_pShader = inst->m_ListShader.find(shader_type)->second;
 	m_pTexture = inst->m_pTextureManager->GetTextureMap()->find(texture_name)->second;
 
-	auto testo = CD3DX12_RESOURCE_DESC::Buffer(64);
+	//auto testo = CD3DX12_RESOURCE_DESC::Buffer(64);
+	auto testo = CD3DX12_RESOURCE_DESC::Buffer(256);
 	CD3DX12_HEAP_PROPERTIES test = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
 	inst->device->CreateCommittedResource(
 		&test,
@@ -51,7 +53,8 @@ void MeshRenderer::InitMeshRenderer(DirectX12Instance* inst, std::string type, s
 		D3D12_RESOURCE_STATE_GENERIC_READ,
 		nullptr,
 		IID_PPV_ARGS(&m_pConstantBufferGPU));
-	
+
+	m_pConstantBufferGPU->Map(0, nullptr, reinterpret_cast<void**>(&m_pConstantBufferData));
 
 };
 
@@ -69,11 +72,7 @@ void MeshRenderer::InitMeshRenderer(DirectX12Instance* inst, std::string type, s
 
 void MeshRenderer::UpdateConstantBuffer(DirectX::XMFLOAT4X4 worldViewProjMatrix) {
 
-	m_pConstantBufferGPU->Map(0, nullptr, reinterpret_cast<void**>(&m_pConstantBufferData));
-
 	memcpy(m_pConstantBufferData, &worldViewProjMatrix, sizeof(worldViewProjMatrix));
-	m_pConstantBufferGPU->Unmap(0, nullptr);
-
 }
 
 #pragma endregion
